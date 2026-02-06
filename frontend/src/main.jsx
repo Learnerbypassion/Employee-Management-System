1a5cde233f1fe002d565d26cda99e1a3c47c2e40
Soham Bhattacharya <callmedad002022@gmail.com>
2026-02-06 11:12:10 +0530

    Uncomment server log forwarding for development

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import AuthProvider from './context/AuthProvider.jsx'
 
-// // enable forwarding console logs to backend dev server terminal (DEV ONLY)
-// if (import.meta.env.MODE === 'development') {
-//   import('./utils/serverLogger').then(mod => mod.enableServerConsole());
-// }
+// enable forwarding console logs to backend dev server terminal (DEV ONLY)
+if (import.meta.env.MODE === 'development') {
+  import('./utils/serverLogger').then(mod => mod.enableServerConsole());
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
